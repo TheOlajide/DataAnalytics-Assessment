dfecf5ffc2fa51eea2d6eb7963eac6329d6fcf56
Olajide Abdullateef <olajideabdullateef@gmail.com>
2025-05-19 09:23:11 +0100

    Update Assessment_Q2.sql

--- Assessment_Q2.sql
+++ Assessment_Q2.sql
@@ -1,14 +1,16 @@
 /*This query calculates the average number of transactions per customer per month without regard for year.*/
 WITH table1 AS (
 SELECT
- users.id AS customer, EXTRACT(MONTH FROM transaction_date) AS month, COUNT(*) AS total_txn
+ users.id AS customer,
+ EXTRACT(MONTH FROM transaction_date) AS month, COUNT(*) AS total_txn
 FROM
  users_customuser AS users
 JOIN
  savings_savingsaccount AS savings
 ON
  savings.owner_id = users.id
-GROUP BY users.id,EXTRACT(MONTH FROM transaction_date)
+GROUP BY
+ users.id,EXTRACT(MONTH FROM transaction_date)
 ),
 table2 AS (
 SELECT
@@ -21,10 +23,12 @@ SELECT avg_transactions_per_month, customer,
        CASE WHEN avg_transactions_per_month >=10 THEN 'High Frequency'
             WHEN avg_transactions_per_month BETWEEN 3 AND 9.999 THEN 'Medium Frequency'
             ELSE  'Low Frequency' END AS frequency_category
-FROM table2
+FROM
+ table2
 )
 SELECT
- avg_transactions_per_month, COUNT(*) AS customer_count , frequency_category
+ avg_transactions_per_month,
+ COUNT(*) AS customer_count , frequency_category
 FROM
  table3
 GROUP BY
